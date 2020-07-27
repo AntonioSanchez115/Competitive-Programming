5710bb4b0b53e212c56e2a200fea82065bf31bb0
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-07-26 22:18:53 -0500

    1924 / Beginner

---
+++ Java Solutions/URI/VitóriaandHerIndecision.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author asus
+ */
+public class VitóriaandHerIndecision {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer c = Integer.parseInt(br.readLine());
+        while (c > 0) {
+            String str = br.readLine();
+            c--;
+        }
+        System.out.println("Ciencia da Computacao");
+    }
+
+}
