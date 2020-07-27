de1e66aed2b21e498091f839cc46727a969e3f42
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-07-26 22:23:07 -0500

    1930 / Beginner

---
+++ Java Solutions/URI/ElectricalOutlet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author asus
+ */
+public class ElectricalOutlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+        int d = Integer.parseInt(s[3]);
+        System.out.println((a + b + c - 3) + d);
+    }
+
+}
