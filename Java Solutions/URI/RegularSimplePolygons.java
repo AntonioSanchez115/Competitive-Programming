170eb8fe3325525e562bf9b2f372ea4ae1841336
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-07-27 22:16:01 -0500

    1959 / Beginner

---
+++ Java Solutions/URI/RegularSimplePolygons.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author asus
+ */
+public class RegularSimplePolygons {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+        long a = Long.parseLong(s[0]);
+        long b = Long.parseLong(s[1]);
+        System.out.println(a * b);
+    }
+
+}
