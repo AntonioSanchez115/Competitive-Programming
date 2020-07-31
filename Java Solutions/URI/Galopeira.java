036739d4085545e4c4b7ca90aa1bd36bc17fe0ae
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-07-30 19:49:31 -0500

    2147 / Beginner

---
+++ Java Solutions/URI/Galopeira.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author asus
+ */
+public class Galopeira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String s = br.readLine();
+            double x = 0;
+            for (int i = 0; i < s.length(); i++) {
+                x += 0.01;
+            }
+            System.out.printf("%.2f\n", x);
+            t--;
+        }
+    }
+
+}
