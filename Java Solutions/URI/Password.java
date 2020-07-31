d556b2bb14f452180935d37be75da1916e66f37c
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-07-30 19:48:48 -0500

    2146 / Beginner

---
+++ Java Solutions/URI/Password.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ *
+ * @author asus
+ */
+public class Password {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            int n = Integer.parseInt(s);
+            System.out.println(n - 1);
+        }
+    }
+
+}
