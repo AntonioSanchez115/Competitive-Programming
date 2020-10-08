743fac7f5c8c5a02dbb6fcd4da08370295c045ec
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-10-08 13:57:20 -0500

    Beginner

---
+++ C++ Solutions/URI/1041 - Coordinates of a Point.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cin>>x>>y;
+    if(x+y==0) cout<<"Origem\n";
+    else if(!x) cout<<"Eixo Y\n";
+    else if(!y) cout<<"Eixo X\n";
+    else if(x>0 && y>0) cout<<"Q1\n";
+    else if(x<0 && y>0) cout<<"Q2\n";
+    else if(x<0 && y<0) cout<<"Q3\n";
+    else cout<<"Q4\n";
+    return 0;
+}
