ab549ff07d7cceac7625d957ee32a7fe13b45dbc
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-10-13 14:56:10 -0500

    Beginner

---
+++ C++ Solutions/URI/1044 - Multiples.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<((a%b==0 || b%a==0) ? "Sao Multiplos\n" : "Nao sao Multiplos\n");
+    return 0;
+}
