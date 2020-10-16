c18270133b2aa5e010addba5f6522272a53d74c7
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-10-15 23:33:26 -0500

    Beginner

---
+++ C++ Solutions/URI/1052 - Month.cpp
@@ -0,0 +1,21 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    int main()
+    {
+        int n;
+        cin>>n;
+        if(n==1) cout<<"January\n";
+        if(n==2) cout<<"February\n";
+        if(n==3) cout<<"March\n";
+        if(n==4) cout<<"April\n";
+        if(n==5) cout<<"May\n";
+        if(n==6) cout<<"June\n";
+        if(n==7) cout<<"July\n";
+        if(n==8) cout<<"August\n";
+        if(n==9) cout<<"September\n";
+        if(n==10) cout<<"October\n";
+        if(n==11) cout<<"November\n";
+        if(n==12) cout<<"December\n";
+        return 0;
+    }
