e7eb76955f101cddd8dca39f94715de2bf362955
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-10-26 13:49:48 -0600

    Beginner

---
+++ C++ Solutions/URI/1114 - Fixed Password.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n && n!=2002) cout<<"Senha Invalida\n";
+    cout<<"Acesso Permitido\n";
+
+    return 0;
+}
