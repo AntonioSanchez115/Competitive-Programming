fc6d3e94248e409afe874bef7aa544a7c8368650
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-11 23:10:17 -0600

    Beginner

---
+++ C++ Solutions/URI/1187 - Top Area.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x, sum=0.0;
+    char c;
+    cin >> c;
+    for(int i=0;i<12;i++){
+        for(int j=0;j<12;j++){
+            cin >> x;
+            if(i<5 && (j>i && j<11-i)) sum += x;
+        }
+    }
+    printf("%.1f\n",((c == 'S') ? (sum) : (sum/30.0)));
+    return 0;
+}
