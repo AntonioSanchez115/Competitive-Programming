cf7a1b7ff099406bc81235667b7719eca5eff20f
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-11 23:01:35 -0600

    Beginner

---
+++ C++ Solutions/URI/1177 - Array Fill II.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i=0;i<1000;i++) cout << "N[" << i << "] = " << i%t << '\n';
+    return 0;
+}
