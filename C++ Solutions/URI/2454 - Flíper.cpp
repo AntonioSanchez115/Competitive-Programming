2761415a06f1281ebdd3550f3c75d5a2db32f767
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-12 23:17:53 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/2454 - Flíper.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p,r;
+    cin >> p >> r;
+    if(p) cout << ((r) ? "A\n" : "B\n");
+    else cout << "C\n";
+    return 0;
+}
