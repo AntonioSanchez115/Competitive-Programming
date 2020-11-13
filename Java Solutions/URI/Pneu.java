deea6d4c4e1abe05713c5468ea969097a38c9df8
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-12 22:57:46 -0600

    2374 / Ad-Hoc

---
+++ Java Solutions/URI/Pneu.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Pneu {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        System.out.println(n - m);
+    }
+
+}
