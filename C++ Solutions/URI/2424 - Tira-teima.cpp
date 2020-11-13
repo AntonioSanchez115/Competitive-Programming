38cb1d7046ec47275f877e5c7a595769d20dc8f6
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-12 23:35:00 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/2424 - Tira-teima.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin >> x >> y;
+    cout << (((x>=0 && x<=432) && (y>=0 && y<=468)) ? "dentro\n" : "fora\n");
+    return 0;
+}
