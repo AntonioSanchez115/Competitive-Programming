58431cac8fa1f75a4fa53e1dab20c255f85a4359
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-13 23:39:44 -0600

    2413 / Ad-Hoc

---
+++ Java Solutions/URI/BuscanaInternet.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class BuscanaInternet {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4);
+    }
+
+}
