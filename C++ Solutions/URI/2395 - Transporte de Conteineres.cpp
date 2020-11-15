d62ff58119a46d07a84750d33752514a3807b2a4
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2020-11-14 23:38:10 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/2395 - Transporte de Conteineres.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long a,b,c,x,y,z;
+    cin >> a >> b >> c >> x >> y >> z;
+    cout << ((long)(x/a)) * ((long)(y/b)) * ((long)(z/c)) << '\n';
+    return 0;
+}
