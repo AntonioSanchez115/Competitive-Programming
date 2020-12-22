9051129385ea13ba9eb3e2e16c06bd6072ab45db
Juan Antonio Sanchez <juanantoniosanchez845@gmail.com>
2020-12-22 01:47:14 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/2175 - What is the Fastest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double O,B,I;
+    cin >> O >> B >> I ;
+    if(O < B && O < I) cout << "Otavio\n";
+    else if(B < O && B < I) cout << "Bruno\n";
+    else if(I < O && I < B) cout << "Ian\n";
+    else cout << "Empate\n";
+    return 0;
+}
