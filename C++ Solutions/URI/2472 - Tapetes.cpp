8874482798913887990d7e6682d7450d6c3e31c1
Juan Antonio Sanchez <juanantoniosanchez845@gmail.com>
2020-12-27 09:52:05 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/2472 - Tapetes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+
+int main()
+{
+    lli l,n;
+    cin >> l >> n;
+    cout << (l-(n-1))*(l-(n-1))+(n-1) << '\n';
+    return 0;
+}
