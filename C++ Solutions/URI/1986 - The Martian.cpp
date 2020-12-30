5354f9984995a3aa9a2fad1416848f60144ea956
Juan Antonio Sanchez <juanantoniosanchez845@gmail.com>
2020-12-30 01:43:50 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/1986 - The Martian.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        scanf("%x",&a);
+        printf("%c",a);
+    }
+    cout << '\n';
+    return 0;
+}
