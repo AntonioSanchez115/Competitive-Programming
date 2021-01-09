3b48b853403f4866cc483e5b81ee1815145db24c
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-01-09 10:41:59 -0600

    Ad-Hoc

---
+++ C++ Solutions/URI/1612 - Little Ant.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        cout << (int)ceil((double)n/2) << '\n';
+    }
+    return 0;
+}
