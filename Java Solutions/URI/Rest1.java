aff086d6406c9a910fb75536d0b0f857d0a46ca2
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-04-21 23:57:48 -0500

    3091 / Beginner

---
+++ Java Solutions/URI/Rest1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Rest1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a % b);
+    }
+
+}
