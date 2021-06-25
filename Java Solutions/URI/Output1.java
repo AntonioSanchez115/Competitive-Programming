aceabb52a74ac1c4fea613206c6b3fa22a681ca0
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-06-25 16:23:17 -0500

    2727 / Beginner

---
+++ Java Solutions/URI/Output1.java
@@ -0,0 +1,13 @@
+public class Output1 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("---------------------------------------");
+    }
+
+}
