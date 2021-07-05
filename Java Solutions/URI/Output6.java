0803daa188865805220375e58f50d49dfa4840fb
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-04 23:25:53 -0500

    Beginner

---
+++ Java Solutions/URI/Output6.java
@@ -0,0 +1,14 @@
+public class Output6 {
+
+    public static void main(String[] args) {
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<    AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO NO URI    >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<          AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO           >");
+    }
+
+}
