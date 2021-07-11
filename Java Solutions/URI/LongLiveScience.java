0e2ac12c4dd0a06b53f71b579c621ace6b01323e
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-10 20:45:28 -0500

    3157 / Beginner

---
+++ Java Solutions/URI/LongLiveScience.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class LongLiveScience {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println("Nao se refuta ciencia com opiniao");
+    }
+
+}
