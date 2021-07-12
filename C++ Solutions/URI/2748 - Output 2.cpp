f3ac8ec0de0486daa260d248e6a02ed77ec660f5
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-11 23:44:59 -0500

    Beginner

---
+++ C++ Solutions/URI/2748 - Output 2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "---------------------------------------\n";
+    cout << "|        Roberto                      |\n";
+    cout << "|                                     |\n";
+    cout << "|        5786                         |\n";
+    cout << "|                                     |\n";
+    cout << "|        UNIFEI                       |\n";
+    cout << "---------------------------------------\n";
+    return 0;
+}
