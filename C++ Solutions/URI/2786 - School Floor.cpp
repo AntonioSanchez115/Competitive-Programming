94fd4cc4e28bb2a0b24575ab66e5b2559a766e2f
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-19 17:00:29 -0500

    Beginner

---
+++ C++ Solutions/URI/2786 - School Floor.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double l, c;
+    cin >> l >> c;
+    cout << ((l - 1) * (c - 1)) + l * c << '\n';
+    cout << (l - 1) * 2 + (c - 1) * 2 << '\n';
+    return 0;
+}
