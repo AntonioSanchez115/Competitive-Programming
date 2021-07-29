bdb572e10af165ee1b84a502984a2cc1b141a48a
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-28 21:57:42 -0500

    Beginner

---
+++ C++ Solutions/URI/3302 - Correct Answer.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        cout << "resposta " << i << ": " << x << '\n';
+    }
+    return 0;
+}
