de063e6b13d208263672381b07e82643de4bf3a8
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-07-31 23:25:50 -0500

    Beginner

---
+++ C++ Solutions/URI/2717 - Elf Time.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << (((a + b) <= n) ? "Farei hoje!" : "Deixa para amanha!") << '\n';
+    return 0;
+}
