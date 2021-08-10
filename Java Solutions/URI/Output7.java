a8fbd457a6c83c88b8c868eda931425b58fb771a
Juan Antonio Sanchez <sanchezantonio.115@gmail.com>
2021-08-09 22:43:51 -0500

    Beginner

---
+++ Java Solutions/URI/Output7.java
@@ -0,0 +1,9 @@
+public class Output7 {
+
+    public static void main(String[] args) {
+        for (int i = 97; i < 123; i++) {
+            System.out.println(i + " e " + (char) i);
+        }
+    }
+
+}
