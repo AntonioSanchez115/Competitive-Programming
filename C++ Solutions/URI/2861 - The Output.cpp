44c9a5ed2a14e7d2104f268f7944b9655f759c30
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2021-09-13 17:40:48 -0500

    Beginner

---
+++ C++ Solutions/URI/2861 - The Output.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    string str;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        getline(cin, str);
+        cout << "gzuz\n";
+    }
+    return 0;
+}
