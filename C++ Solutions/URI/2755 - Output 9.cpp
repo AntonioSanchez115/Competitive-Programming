dc96c9b9a990b53638323d1e183c3c3a49d754b6
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2021-10-12 23:56:22 -0500

    Beginner

---
+++ C++ Solutions/URI/2755 - Output 9.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "\"Ro'b'er\tto\\/\"\n";
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)\n";
+    cout << "(^_-) (-_-) (-_^)\n";
+    cout << "(\"_\") ('.')\n";
+    return 0;
+}
