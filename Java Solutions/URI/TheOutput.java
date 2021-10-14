69eda337917425e8b02f59332973aaaf1fb26ec0
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2021-10-13 23:50:03 -0500

    2861 / Beginner

---
+++ Java Solutions/URI/TheOutput.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class TheOutput {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        String str;
+        while ((t--) > 0) {
+            str = br.readLine();
+            System.out.println("gzuz");
+        }
+    }
+
+}
