907250e4e4feb1f8c794952f325a87a6af91f167
AntonioSanchez115 <sanchezantonio.115@gmail.com>
2021-10-15 23:58:05 -0500

    2755 / Beginner

---
+++ Java Solutions/URI/Output9.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Output9 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("\"Ro'b'er\tto\\/\"");
+        System.out.println("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+        System.out.println("(^_-) (-_-) (-_^)");
+        System.out.println("(\"_\") ('.')");
+    }
+
+}
